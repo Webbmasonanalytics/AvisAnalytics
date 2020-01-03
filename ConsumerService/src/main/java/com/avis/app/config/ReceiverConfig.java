4c5f785b234619de3a6824f3776c6bc6ca2e5105
svaduka <sainagaraju.vaduka@gmail.com>
2020-01-03 11:00:05 +0530

    Added SSL properties to kafka

--- ConsumerService/src/main/java/com/avis/app/config/ReceiverConfig.java
+++ ConsumerService/src/main/java/com/avis/app/config/ReceiverConfig.java
@@ -3,7 +3,9 @@ package com.avis.app.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +19,7 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 @EnableKafka
 public class ReceiverConfig {
 
-    @Value("${group.id}")
+    @Value("${kafka.group.id}")
     private String groupId;
     @Value("${kafka.bootstrap.servers}")
     private String kafkaServers;
@@ -25,7 +27,22 @@ public class ReceiverConfig {
     private String offsetRest;
     @Value("${kafka.auto.commit}")
     private String autoCommit;
-
+    
+    @Value("${kafka.security.protocol}")
+    private String securityProtocol;
+    
+    @Value("${kafka.ssl.enabled.protocols}")
+    private String sslEnabledProtocols;
+    
+    @Value("${kafka.ssl.truststore.type}")
+    private String sslTrustStoreType;
+    
+    @Value("${kafka.ssl.cipher.suites}")
+    private String sslCipherSuites;
+    
+    @Value("${kafka.ssl.endpoint.identification.algorithm}")
+    private String sslEndPointIdentificationAlgorithm;
+    
 
     @Bean
     public Map<String,Object> consumerConfigs() {
@@ -36,6 +53,16 @@ public class ReceiverConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offsetRest);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, autoCommit);
+        
+        //SSL
+
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
+        props.put(SslConfigs.SSL_ENABLED_PROTOCOLS_CONFIG,sslEnabledProtocols);
+        props.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG,sslTrustStoreType);
+        props.put(SslConfigs.SSL_CIPHER_SUITES_CONFIG,sslCipherSuites);
+        props.put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG,sslEndPointIdentificationAlgorithm);
+
+        
 
         return props;
     }
