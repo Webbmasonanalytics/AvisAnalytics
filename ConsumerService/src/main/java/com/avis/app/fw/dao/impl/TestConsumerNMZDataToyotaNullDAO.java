e91ff9cd37a93b1af4ddb8c5d9822d1369e7cdba
svaduka <sainagaraju.vaduka@gmail.com>
2020-02-11 14:53:23 +0530

    Added upload logic

--- ConsumerService/src/main/java/com/avis/app/fw/dao/impl/TestConsumerNMZDataToyotaNullDAO.java
+++ ConsumerService/src/main/java/com/avis/app/fw/dao/impl/TestConsumerNMZDataToyotaNullDAO.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.avis.app.nmz.kafka.model.NormalizedMsg;
+import com.avis.app.util.AwsS3Util;
+import com.avis.app.util.FileUtils;
 import com.avis.app.util.JsonParserUtil;
 
 @Component
@@ -18,14 +20,26 @@ public class TestConsumerNMZDataToyotaNullDAO extends DAO
 
 	
 	@Autowired JsonParserUtil jsonParserUtil;
+	
+	@Autowired
+	FileUtils fileUtils;
+
+	@Autowired
+	AwsS3Util awsS3Util;
 	@Value("${telemetry.toyota.nmz.test.aws.directory.inbox}")
 	private String inboxDir;
+	
+	@Value("${telemetry.toyota.nmz.test.aws.fileName}")
+	private String fileName;
 	
 	@Value("${telemetry.toyota.nmz.test.aws.bucketName}")
 	private String testBucket;
 
-	@Value("${telemetry.toyota.raw.localfile.directory}")
+	@Value("${telemetry.toyota.nmz.test.localfile.directory}")
 	private String localDir;
+	
+	@Value("${telemetry.toyota.nmz.test.aws.fileName.format}")
+	private String format;
 	
 	@Value("${source.toyota}")
 	private String toyotaSource;
@@ -33,14 +47,24 @@ public class TestConsumerNMZDataToyotaNullDAO extends DAO
 	@Override
 	public boolean insertRecord(ConsumerRecord<String, String> record) throws Exception 
 	{
-		logger.debug("In TestConsumerNMZDataToyotaNullDAO");
+		logger.info("In TestConsumerNMZDataToyotaNullDAO");
+		long currentTimeInMillis = System.currentTimeMillis();
 		NormalizedMsg dao = (NormalizedMsg)jsonParserUtil.getDataObject(record.value(), NormalizedMsg.class);
 		if(StringUtils.equalsIgnoreCase(dao.getSource(),toyotaSource)) {
 			if(dao.getTelemetry()==null) {
-				logger.debug("Writing data for partition {}, offset {}", record.partition(), record.offset());
-				logger.info(record.toString());
+				int partition=record.partition();
+				long offset=record.offset();
+				logger.debug("Writing data for partition {}, offset {}", partition, offset);
+				String generatedFileName = fileName + "_" + partition + "_" + offset + "." + format;
+				String localFile = localDir+"/"+generatedFileName;
+				logger.debug("Writing to local file {}",localFile);
+				fileUtils.writeDataToFile(record.toString(), localFile, false);
+				final String s3PutObjectKey =inboxDir + "/" + generatedFileName;
+				awsS3Util.uploadObject(testBucket, s3PutObjectKey, localFile);
 			}
 		}
+		long endTimeInMillis = System.currentTimeMillis();
+		logger.debug("Total time in millis:{}",(endTimeInMillis-currentTimeInMillis));
 		return true;
 	}
 
